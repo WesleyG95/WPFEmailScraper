b888ccf9827a807763f003f22d826d7382378dd3
WesleyG95 <WesleyG95@hotmail.com>
2016-01-20 09:23:37 -0500

    Initial Commit

---
+++ EmailScraper.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Net.Mail;
+
+namespace WpfEmailScraper
+{
+    class EmailScraper
+    {
+        //Private Class Members
+        private HashSet<MailAddress> _results = new HashSet<MailAddress>();
+
+        //Public Class Properties
+        public HashSet<MailAddress> Results
+        {
+            get
+            {
+                return this._results;
+            }
+        }
+
+        //Public Methods
+        public void Scrape(string url)
+        {
+            WebClient client = new WebClient();
+
+            try
+            {
+                string result = client.DownloadString(url);
+
+                //Use A Search To Find Emails In Result
+                Regex regex = new Regex(@"[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,6}", RegexOptions.IgnoreCase);
+                MatchCollection matches = regex.Matches(result);
+
+                //Store Found Emails In The Results
+                foreach (Match match in matches)
+                {
+                    this._results.Add(new MailAddress(match.Value));
+                }
+            }
+            catch
+            {
+                //What Should I Do Here?
+                //Maybe Nothing for Now
+            }
+        }
+    }
+}
---
+++ LinkScraper.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text.RegularExpressions;
+using HtmlAgilityPack;
+
+namespace WpfEmailScraper
+{
+    class LinkScraper
+    {
+        //Private Class Members
+        private HashSet<Uri> _results = new HashSet<Uri>();
+
+        //Public Class Properties
+        public HashSet<Uri> Results
+        {
+            get
+            {
+                return this._results;
+            }
+        }
+
+        //Public Methods
+        public void Scrape(string url)
+        {
+            WebClient client = new WebClient();
+
+            try
+            {
+
+                HtmlWeb hw = new HtmlWeb();
+                HtmlDocument doc = hw.Load(url);
+
+                foreach(HtmlNode match in doc.DocumentNode.SelectNodes("//a[@href]"))
+                {
+                    try
+                    {
+                        this._results.Add(new Uri(match.Attributes["href"].Value));
+                    }
+                    catch
+                    {
+
+                    }
+                }
+            }
+            catch
+            {
+                //What Should I Do Here?
+                //Maybe Nothing for Now
+            }
+        }
+    }
+}
